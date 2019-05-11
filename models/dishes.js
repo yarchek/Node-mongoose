8725e2c683594e651d290847623b5704f50dfc3f
yarchek <y.chekotun-2023@kpi.ua>
2019-05-11 17:37:28 +0300

    Mongoose ODM

---
+++ models/dishes.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type:String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [ commentSchema ]
+    }, {
+        timestamps: true
+});
+
+let Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
